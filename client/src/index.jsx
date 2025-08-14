864d6ee90ef78312f62e0d75f77a5a278c4067f2
rspoonmore <rspoonmore@gmail.com>
2025-08-14 11:05:41 -0500

    switch client over to vite and install tailwindcss

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css';
 import routes from './routes.jsx';
-import { AuthProvider } from './context/AuthContext.js';
+import { AuthProvider } from './context/AuthContext';
 
 const router = createBrowserRouter(routes);
 const root = ReactDOM.createRoot(document.getElementById('root'));
