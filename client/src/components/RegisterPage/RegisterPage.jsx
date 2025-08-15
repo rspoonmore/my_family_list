28bdf515c3af529208c8d3df27b5b9b825a4abf0
rspoonmore <rspoonmore@gmail.com>
2025-08-15 09:43:25 -0500

    Setup structure for register page

---
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,14 @@
+import ('./RegisterPage.css');
+import Header from '../Header/Header';
+
+const RegisterPage = () => {
+    
+    return (
+        <div>
+            <Header />
+            <div>Register Here</div>
+        </div>
+    )
+};
+
+export default RegisterPage;
--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,10 +1,15 @@
 import StartPage from "./components/StartPage/StartPage.jsx";
+import RegisterPage from "./components/RegisterPage/RegisterPage.jsx";
 
 const routes = [
     {
         path: '/',
-        element: <StartPage></StartPage>
+        element: <StartPage />
     },
+    {
+        path: '/register',
+        element: <RegisterPage />
+    }
 ];
 
 export default routes;
