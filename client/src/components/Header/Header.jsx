c45aa335afcea4b002896bfbe2257c508e6ee641
rspoonmore <rspoonmore@gmail.com>
2025-08-26 14:32:05 -0500

    Move dropdowns to Header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,8 +3,13 @@ import { Link } from 'react-router-dom'
 import { useState, useContext } from 'react'
 import { AuthContext } from '../../context/AuthContext'
 import LoginForm from '../Forms/LoginForm.jsx'
+import UserBadge from '../Badges/UserBadge.jsx'
+import Icon from '@mdi/react'
+import { mdiMenu, mdiMenuOpen } from '@mdi/js'
 
-const Header = ({dropdownLogo = null, userBadge = () => {return <></>}}) => {
+const Header = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [showAccountSidebar, setShowAccountSidebar] = useState(false);
     const [showLoginForm, setShowLoginForm] = useState(false);
     const { currentUser, setCurrentUser } = useContext(AuthContext);
     const apiUrl = import.meta.env.VITE_API_URL;
@@ -45,15 +50,82 @@ const Header = ({dropdownLogo = null, userBadge = () => {return <></>}}) => {
         return <button className='btn' onClick={logout}>Log Out</button>
     }
 
+    // Left Sidebar Logo
+    const dropdownLogo = () => {
+        const logoPath = showDropdown ? mdiMenuOpen : mdiMenu;
+        const clicked = () => {
+            if(setShowDropdown) {
+                setShowDropdown(prev => !prev)
+            }
+        }
+        return <Icon path={logoPath} size={1} color={"white"} onClick={clicked}/>
+    }
+
+    // Function when left sidebar links are clicked
+    function sideBarButtonHit() {
+        setShowDropdown(false);
+        setShowAccountSidebar(false);
+    }
+
+    // Left Sidebar
+    const sideBar = () => {
+        if(!showDropdown) {return <></>}
+
+        const adminPage = () => {
+            if(!currentUser) {return <></>}
+            if(!currentUser.admin) {return <></>}
+            return <Link to='/admin-page' onClick={sideBarButtonHit}>Admin Page</Link>
+        }
+
+        return (
+            <div className='sidebar'>
+                <Link to='/register' onClick={sideBarButtonHit}>Register</Link>
+                {adminPage()}
+            </div>
+        )
+    }
+
+    // Function when right sidebar links are clicked
+    function rightSideBarButtonHit() {
+        setShowDropdown(false);
+        setShowAccountSidebar(false);
+    }
+
+    // Function when right sidebar links are clicked
+    const accountLinks = () => {
+        if(!showAccountSidebar) {return <></>}
+        if(!currentUser) {return <></>}
+        return (
+            <div id='account-links'>
+                <Link to={`/users/${currentUser.userid}/update`} state={{user: currentUser}} onClick={rightSideBarButtonHit}>Update User</Link>
+                <Link to={`/users/${currentUser.userid}/password`} onClick={rightSideBarButtonHit}>Change Password</Link>
+            </div>
+        )
+    }
+
+    // Header badge clicked
+    const userBadgeClicked = () => {
+        setShowAccountSidebar(prev => {
+            return !prev
+        })
+    }
+
+    // Header badge
+    const userBadge = () => {
+        return <UserBadge onClick={userBadgeClicked}/>
+    }
+
     return (
         <div className='header'>
             <div id='header-link-div'>
                 <Link to='/' className='header-link'>Home</Link>
                 {dropdownLogo()}
+                {sideBar()}
             </div>
             <div id='header-button-div'>
                 {userBadge()}
                 {currentUser ? logoutButton() : loginButton()}
+                {accountLinks()}
             </div>
             <LoginForm 
                 showLoginForm={showLoginForm}
--- client/src/components/PageShell/PageShell.jsx
+++ client/src/components/PageShell/PageShell.jsx
@@ -4,14 +4,11 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext'
 import Header from '../Header/Header.jsx'
 import { clearCurrentUserIfNoCookie, clearCookiesIfNoCurrentUser } from '../../cookies/CookieHandler.js';
-import UserBadge from '../Badges/UserBadge.jsx'
-import Icon from '@mdi/react'
-import { mdiMenu, mdiMenuOpen } from '@mdi/js'
+
 
 
 const PageShell = ({mainView=null}) => {
-    const [showDropdown, setShowDropdown] = useState(false);
-    const [showAccountSidebar, setShowAccountSidebar] = useState(false);
+    
     const { currentUser, setCurrentUser } = useContext(AuthContext);
 
     const loadScreen = () => {
@@ -22,71 +19,6 @@ const PageShell = ({mainView=null}) => {
     }
 
     useEffect(loadScreen, [])
-
-    // Left Sidebar Logo
-    const dropdownLogo = () => {
-        const logoPath = showDropdown ? mdiMenuOpen : mdiMenu;
-        const clicked = () => {
-            if(setShowDropdown) {
-                setShowDropdown(prev => !prev)
-            }
-        }
-        return <Icon path={logoPath} size={1} color={"white"} onClick={clicked}/>
-    }
-
-    // Function when left sidebar links are clicked
-    function sideBarButtonHit() {
-        setShowDropdown(false);
-        setShowAccountSidebar(false);
-    }
-
-    // Left Sidebar
-    const sideBar = () => {
-        if(!showDropdown) {return <></>}
-
-        const adminPage = () => {
-            if(!currentUser) {return <></>}
-            if(!currentUser.admin) {return <></>}
-            return <Link to='/admin-page' onClick={sideBarButtonHit}>Admin Page</Link>
-        }
-
-        return (
-            <div className='sidebar'>
-                <Link to='/register' onClick={sideBarButtonHit}>Register</Link>
-                {adminPage()}
-            </div>
-        )
-    }
-
-    // Function when right sidebar links are clicked
-    function rightSideBarButtonHit() {
-        setShowDropdown(false);
-        setShowAccountSidebar(false);
-    }
-
-    // Function when right sidebar links are clicked
-    const accountLinks = () => {
-        if(!showAccountSidebar) {return <></>}
-        if(!currentUser) {return <></>}
-        return (
-            <div id='account-links'>
-                <Link to={`/users/${currentUser.userid}/update`} state={{user: currentUser}} onClick={rightSideBarButtonHit}>Update User</Link>
-                <Link to={`/users/${currentUser.userid}/password`} onClick={rightSideBarButtonHit}>Change Password</Link>
-            </div>
-        )
-    }
-
-    // Header badge clicked
-    const userBadgeClicked = () => {
-        setShowAccountSidebar(prev => {
-            return !prev
-        })
-    }
-
-    // Header badge
-    const userBadge = () => {
-        return <UserBadge onClick={userBadgeClicked}/>
-    }
 
     const footer = () => {
         return (
@@ -100,10 +32,8 @@ const PageShell = ({mainView=null}) => {
     const pageShell = () => {
         return (
             <div id='shell-container'>
-                <Header dropdownLogo={dropdownLogo} userBadge={userBadge}></Header>
+                <Header />
                 <div id='shell-content'>
-                    {sideBar()}
-                    {accountLinks()}
                     {mainView ? mainView() : <div>Main View</div>}
                 </div>
                 {footer()}
