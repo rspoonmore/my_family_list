b1f9fb60cf369bfeae53f1bba929ca3a85b434a6
rspoonmore <rspoonmore@gmail.com>
2025-10-01 14:11:51 -0500

    sameSite instead of SameSite

--- server/passport/cookieOptions.js
+++ server/passport/cookieOptions.js
@@ -2,7 +2,6 @@ exports.cookieOptions = {
 	maxAge: 1000 * 60 * 60 * 24 * 30,
 	httpOnly: false,
 	secure: true,
-	// sameSite: 'none',
-	SameSite: "None"
+	sameSite: 'None'
     // 'Access-Control-Allow-Credentials': true
 };
