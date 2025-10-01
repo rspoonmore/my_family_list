a3d5792cc169b0646b8640098b6fd0fb804f2d11
rspoonmore <rspoonmore@gmail.com>
2025-10-01 11:35:20 -0500

    Simplify PageShell

--- client/src/components/PageShell/PageShell.jsx
+++ client/src/components/PageShell/PageShell.jsx
@@ -1,15 +1,12 @@
 import './PageShell.css';
-import { useState, useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext'
 import Header from '../Header/Header.jsx'
-import { clearCurrentUserIfNoCookie, clearCookiesIfNoCurrentUser } from '../../cookies/CookieHandler.js';
 
 
 
 const PageShell = ({mainView=null}) => {
     
-    const { currentUser, setCurrentUser, isInitialized } = useContext(AuthContext);
+    const { isInitialized } = useContext(AuthContext);
 
     const loadScreen = () => {
         if(!isInitialized) {return}
@@ -30,21 +27,24 @@ const PageShell = ({mainView=null}) => {
         )
     }
 
-    const pageShell = () => {
-        if(!isInitialized) {return <div className='p-10 text-center'>Loading application...</div>;}
-
-        return (
-            <div id='shell-container'>
-                <Header />
-                <div id='shell-content'>
-                    {mainView ? mainView() : <div>Main View</div>}
+    const renderView = () => {
+        if(isInitialized) {
+            return (
+                <div id='shell-container'>
+                    <Header />
+                    <div id='shell-content'>
+                        {mainView ? mainView() : <div>Main View</div>}
+                    </div>
+                    {footer()}
                 </div>
-                {footer()}
-            </div>
-        )
+            )
+        }
+        else {
+            return <div className='p-10 text-center'>Loading application...</div>;
+        }
     }
-
-    return pageShell()
+    
+    return renderView();
     
 }
 
