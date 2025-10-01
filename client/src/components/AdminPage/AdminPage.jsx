294bb554a6380d3688076c0f19ca0a6f6f967d75
rspoonmore <rspoonmore@gmail.com>
2025-10-01 11:58:39 -0500

    change main render of AdminPage to function

--- client/src/components/AdminPage/AdminPage.jsx
+++ client/src/components/AdminPage/AdminPage.jsx
@@ -129,7 +129,7 @@ const AdminPage = () => {
     }
 
     // Generate View
-    const generateView = () => {
+    function generateView() {
         // if(!isInitialized) {return <div className='p-10 text-center'>Loading application...</div>;}
         if(!isInitialized) {return <div className='text-4xl'>Content Loading...</div>}
         if(!updateAllowed) {return <div className="p-8 text-lg text-red-600">You are not an admin, and therefore cannot access this page.</div>}
