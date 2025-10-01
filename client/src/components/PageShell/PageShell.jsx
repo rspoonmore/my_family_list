5ac5a2fd4307f1f15f66d4e3e9a082fffc134c17
rspoonmore <rspoonmore@gmail.com>
2025-10-01 11:40:10 -0500

    Add useEffect back into PageShell

--- client/src/components/PageShell/PageShell.jsx
+++ client/src/components/PageShell/PageShell.jsx
@@ -1,5 +1,5 @@
 import './PageShell.css';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AuthContext } from '../../context/AuthContext'
 import Header from '../Header/Header.jsx'
 
