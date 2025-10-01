ffd3758dbfa44f1d02044539175e5240281ac7a3
rspoonmore <rspoonmore@gmail.com>
2025-10-01 14:05:34 -0500

    SameSite cookieOptions update

--- server/passport/cookieOptions.js
+++ server/passport/cookieOptions.js
@@ -2,7 +2,7 @@ exports.cookieOptions = {
 	maxAge: 1000 * 60 * 60 * 24 * 30,
 	httpOnly: false,
 	secure: true,
-	sameSite: 'none',
-	SameStie: 'None'
+	// sameSite: 'none',
+	SameStie: "None"
     // 'Access-Control-Allow-Credentials': true
 };
